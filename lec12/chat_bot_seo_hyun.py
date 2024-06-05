df817a4d6100bf708348fc9b2f3f4177d2444c45
lifelo <153349640+lifelo@users.noreply.github.com>
2024-06-05 06:30:26 +0000

    chat_bot_seo_hyun

---
+++ lec12/chat_bot_seo_hyun.py
@@ -0,0 +1,13 @@
+import telegram
+ 
+token = '7429318101:AAF28ja25XhaRQAweEpqfclNOLiVLPDVY3E'
+id = "6927708945"
+
+bot = telegram.Bot(token)
+bot.sendMessage(chat_id=id, text="아제발종강시켜주세요그리고돈이나펑펑쓰면서놀러만다니고싶허효제발.")
+# updates = bot.getUpdates()
+# for u in updates:
+#     print(u.message)
+
+
+
